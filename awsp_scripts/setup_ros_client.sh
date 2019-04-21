e87cc8226c6f722b11199e735ed7cf6d87a6591e
David Michalik <david.michalik95@gmail.com>
2019-04-21 18:59:31 +0200

    added a comment on some possible erros with /etc/hosts

--- awsp_scripts/setup_ros_client.sh
+++ awsp_scripts/setup_ros_client.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#  - To actually make it work and connenct other running my two scripts on both sides of the client and server, it is also might be needed to set the `/etc/hosts` file with this line:
+#  - `{SERVER IP} {SERVER_NAME}`
+#  - to get the ip that goes in there do: `hostname -I`
+#  - to get the server name: `whoami` (or in some cases `awsp_pi`)
 ros_master_ip=$1
 echo "--> This script is to setup the CLIENT to be able to connect to remote a ROS master."
 #current_ip=$(hostname -I)
