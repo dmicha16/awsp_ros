9c2e215ccd1f9bb40d94eef5754fed15af56ec91
David Michalik <david.michalik95@gmail.com>
2019-05-01 13:32:14 +0000

    added some files to gitignore, and updates setup scripts

--- awsp_scripts/setup_ros_server.sh
+++ awsp_scripts/setup_ros_server.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 echo "--> This script is to setup the SERVER to be able to connect to remote a ROS master."
-export ROS_MASTER_URI=http://$(hostname -I):11311
-export ROS_IP=$(hostname -I)
+HOSTNAME=`hostname -I`
+HOSTNAME_S="$(echo "${HOSTNAME}" | tr -d '[:space:]')"
+export ROS_MASTER_URI=http://$HOSTNAME_S:11311
+export ROS_IP=$HOSTNAME_S
 echo "--> All environment variables are now set, gonna print those variables now!"
-sleep 2
 printenv | grep ROS_MASTER
 printenv | grep ROS_IP
-echo "--> Gonna run 'roswtf' now, make sure you don't see any errors pop up!"
-sleep 3
-roswtf
+#echo "--> Gonna run 'roswtf' now, make sure you don't see any errors pop up!"
+#sleep 3
+##roswtf
