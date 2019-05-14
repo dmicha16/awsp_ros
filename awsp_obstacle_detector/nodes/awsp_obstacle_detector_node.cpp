ba7455b558c35332b3bbf224ad3910c7ec43dfce
David Michalik <david.michalik95@gmail.com>
2019-05-14 19:27:05 +0000

    the obstacle data is now published and it is possible to set the trigger, echo pins, the distance threshold and the timeout params as arguments to the node

--- awsp_obstacle_detector/nodes/awsp_obstacle_detector_node.cpp
+++ awsp_obstacle_detector/nodes/awsp_obstacle_detector_node.cpp
@@ -7,8 +7,41 @@
 #include <iostream>
 #include <chrono>
 
+bool evaluate_obstacle(double front_obstacle_dist, float distance_thresh)
+{
+    if (front_obstacle_dist < 0)
+        return false;
+
+    else if (front_obstacle_dist > distance_thresh)
+        return false;
+    else
+        return true;
+}
+
 int main(int argc, char **argv)
 {
+    // args TRIGGER, ECHO pins and DISTANCE_THRESH
+    if (argc < 2)
+    {
+        ROS_ERROR("TRIGGER pin not specified.");
+        return 1;
+    }
+    else if (argc < 3)
+    {
+        ROS_ERROR("ECHO pin not specified.");
+        return 1;
+    }
+    else if (argc < 4)
+    {
+        ROS_ERROR("Timeout not specified");
+        return 1;
+    }
+
+    int trigger = atoi(argv[1]);
+    int echo = atoi(argv[2]);
+    float distance_thresh = atof(argv[3]);
+    long timeout = atol(argv[4]);
+
     ROS_INFO("Starting node");
     ros::init(argc, argv, "awsp_obstacle_detector_node");
     ros::NodeHandle n;
@@ -18,42 +51,42 @@ int main(int argc, char **argv)
 
     if (wiringPiSetup() != 0)
     {
-        printf("Failed to wiringPiSetupGpio()\n");
-        return 0;
+        ROS_ERROR("Failed to wiringPiSetup()\n");
     }
 
-    int trigger_ = 23;
-    int echo_ = 24;
-    pinMode(trigger_, OUTPUT);
-    pinMode(echo_, INPUT);
+    ROS_INFO("pint pinMode");
+    pinMode(trigger, OUTPUT);
+    pinMode(echo, INPUT);
     delay(2000);
+    ROS_INFO("pint pinMode2");
 
-    long end_time = 0;
-    long now, travelTimeUsec, timeout;
-    volatile long startTimeUsec;
-    volatile long endTimeUsec;
-    double distanceMeters;
+    long now, travel_time_micro;
+    volatile long start_time_micro;
+    volatile long end_time_micro;
+    double front_obstacle_dist;
 
     while(ros::ok())
     {
-        digitalWrite(trigger_, HIGH);
+        ROS_INFO_ONCE("Started advertising on topic: obstacle_data");
+        digitalWrite(trigger, HIGH);
         delayMicroseconds(10);
-        digitalWrite(trigger_, LOW);
+        digitalWrite(trigger, LOW);
 
-        now=micros();
-        timeout = 30000;
+        now = micros();
 
-        while (digitalRead(echo_) == LOW && micros()-now<timeout);
-        startTimeUsec = micros();
-        while ( digitalRead(echo_) == HIGH );
-        endTimeUsec = micros();
+        while (digitalRead(echo) == LOW && micros() - now < timeout);
+        start_time_micro = micros();
+        while ( digitalRead(echo) == HIGH );
+        end_time_micro = micros();
 
-        travelTimeUsec = endTimeUsec - startTimeUsec;
-        distanceMeters = 100*((travelTimeUsec/1000000.0)*340.29)/2;
+        travel_time_micro = end_time_micro - start_time_micro;
+        front_obstacle_dist = 100*((travel_time_micro/1000000.0)*340.29)/2;
+
+        obstacle_data.front_obstacle_dist = front_obstacle_dist;
+        obstacle_data.front_obstacle = evaluate_obstacle(front_obstacle_dist, distance_thresh);
 
         ros::spinOnce();
         rate.sleep();
-
     }
     return 0;
 }
